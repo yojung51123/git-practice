5de0550799d4c2e3cb2e446623baa5525c3da01e
yojung51123 <ak471125@naver.com>
2025-09-25 09:23:08 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <cctype> 
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    
+    if(s.size() == 4 || s.size() == 6)
+    {
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] < '0' || s[i] > '9')
+            {
+                answer = false;
+                return answer;
+            }
+         }
+    } 
+    else
+    {
+        answer = false;
+        return answer;
+    }
+    return answer;
+}
